3c9da9d994991def04961e61ca64aadef225a5a9
LeeZizz <leedt2003hy@gmail.com>
2025-11-20 16:19:41 +0700

    creat enviroment

---
+++ vitazibe/src/main/java/com/ezi/vitazibe/VitazibeApplication.java
@@ -0,0 +1,13 @@
+package com.ezi.vitazibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitazibeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitazibeApplication.class, args);
+	}
+
+}
---
+++ vitazibe/src/test/java/com/ezi/vitazibe/VitazibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezi.vitazibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitazibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
